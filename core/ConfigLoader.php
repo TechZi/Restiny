1de2fc7521e55bec01031fb785afd28637099184
yeszyy@gmail.com <yeszyy@gmail.com@81ff0231-e788-5423-ead5-9761545eb30d>
2011-05-08 01:17:29 +0000

    清理格式
    git-svn-id: http://restiny.googlecode.com/svn/trunk@30 81ff0231-e788-5423-ead5-9761545eb30d

--- core/ConfigLoader.php
+++ core/ConfigLoader.php
@@ -7,20 +7,21 @@
  * @version $Id$
  *
  */
-
 class ConfigLoader {
+
 	public function loadConfig($configFileName, $configDirectoryPath = '') {
 		if (empty($configDirectoryPath)) {
-			$configDirectoryPath = APP_PATH.DIRECTORY_SEPARATOR.'config';
+			$configDirectoryPath = APP_PATH . DIRECTORY_SEPARATOR . 'config';
 		}
 
 		//TODO 进程级的缓存
-		$configFilePath = $configDirectoryPath . DIRECTORY_SEPARATOR . $configFileName.'.php';
+		$configFilePath = $configDirectoryPath . DIRECTORY_SEPARATOR . $configFileName . '.php';
 
 		if (!file_exists($configFilePath)) {
-			throw new RestinyFileNotFoundException($configFileName.' config file not found', 0);
+			throw new RestinyFileNotFoundException($configFileName . ' config file not found', 0);
 		}
 
 		return require_once $configFilePath;
 	}
+
 }
